f6aa651fb7e6e0f95719c388cb4d65ee0b25ab47
zhaochuninhefei <zhaochun.hefei@qq.com>
2022-12-12 18:30:34 +0800

    修改protoreflect测试用例

--- protobuf/protoreflect/protoreflect_test.go
+++ protobuf/protoreflect/protoreflect_test.go
@@ -2,12 +2,36 @@ package protoreflect
 
 import (
 	"fmt"
-	"gitee.com/zhaochuninhefei/zcutils-go/testdata/myproto-go/owner"
+	"gitee.com/zhaochuninhefei/zcutils-go/protobuf/myproto-go/asset"
+	"gitee.com/zhaochuninhefei/zcutils-go/protobuf/myproto-go/owner"
 	"google.golang.org/protobuf/proto"
 	"testing"
 )
 
-func Test_getFields_success(t *testing.T) {
+func Benchmark(b *testing.B) {
+	asset1 := &asset.BasicAsset{
+		AssetId:    1,
+		AssetName:  "测试资产1",
+		AssetPrice: 108,
+		AssetOwner: &owner.Owner{
+			OwnerId:   0,
+			OwnerName: "张三",
+		},
+		AssetNum:    &asset.BasicAsset_AssetNumInt{AssetNumInt: 123},
+		AssetStatus: asset.BasicAsset_CHANGED,
+	}
+	pb := proto.Message(asset1)
+	for i := 0; i < b.N; i++ {
+		_, err := GetFields(pb)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+	}
+}
+
+func Test_getFields_owner_success(t *testing.T) {
+	fmt.Println("--- Test_getFields_owner_success start ---")
 	owner1 := &owner.Owner{
 		OwnerId:   1,
 		OwnerName: "owner1",
@@ -24,4 +48,32 @@ func Test_getFields_success(t *testing.T) {
 			fmt.Printf("fd: %s\n", fd)
 		}
 	}
+	fmt.Println("--- Test_getFields_owner_success end ---")
+}
+
+func Test_getFields_asset_success(t *testing.T) {
+	fmt.Println("--- Test_getFields_asset_success start ---")
+	asset1 := &asset.BasicAsset{
+		AssetId:    1,
+		AssetName:  "测试资产1",
+		AssetPrice: 108,
+		AssetOwner: &owner.Owner{
+			OwnerId:   0,
+			OwnerName: "张三",
+		},
+		AssetNum:    &asset.BasicAsset_AssetNumInt{AssetNumInt: 123},
+		AssetStatus: asset.BasicAsset_CHANGED,
+	}
+	pb := proto.Message(asset1)
+
+	got, err := GetFields(pb)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if got != nil {
+		for _, fd := range got {
+			fmt.Printf("fd: %s\n", fd)
+		}
+	}
+	fmt.Println("--- Test_getFields_asset_success end ---")
 }
