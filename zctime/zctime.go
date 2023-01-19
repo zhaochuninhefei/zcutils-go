87d36c43f51790276ef709e79ae042acbaf5e2fd
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-01-19 09:39:59 +0800

    开发

--- zctime/zctime.go
+++ zctime/zctime.go
@@ -1,6 +1,6 @@
 package zctime
 
-//goland:noinspection GoSnakeCaseUsage
+//goland:noinspection GoSnakeCaseUsage,GoUnusedConst
 const (
 	TIME_FORMAT_SIMPLE = "20060102150405"
 	TIME_FORMAT_SLASH  = "2006/01/02 15:04:05"
