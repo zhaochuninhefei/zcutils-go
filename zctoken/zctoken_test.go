889a6f65042fbf8d5a80de769ee136741f9d5163
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-01-19 10:32:26 +0800

    开发

--- zctoken/zctoken_test.go
+++ zctoken/zctoken_test.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"gitee.com/zhaochuninhefei/gmgo/sm2"
 	"gitee.com/zhaochuninhefei/gmgo/x509"
+	"strings"
 	"testing"
 	"time"
 )
@@ -70,4 +71,37 @@ func TestBuildTokenWithGM(t *testing.T) {
 	}
 	jsonPayloads, _ := json.Marshal(payloadsAfterCheck)
 	fmt.Printf("jsonPayloads: %s\n", jsonPayloads)
+}
+
+func TestBuildTokenWithGMTimeout(t *testing.T) {
+	// 从pem文件读取私钥
+	privKey, err := x509.ReadPrivateKeyFromPemFile("testdata/pri_key.pem", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// 从pem文件读取公钥
+	pubKey, err := x509.ReadPublicKeyFromPemFile("testdata/pub_key.pem")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	payloads := make(map[string]string)
+	token, err := BuildTokenWithGM(payloads, time.Now().Add(time.Second*1), privKey.(*sm2.PrivateKey))
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("token: %s\n", token)
+
+	time.Sleep(time.Second * 3)
+
+	_, err = CheckTokenWithGM(token, pubKey.(*sm2.PublicKey))
+	if err != nil {
+		if strings.HasPrefix(err.Error(), "[-1]token过期") {
+			fmt.Printf("token超时正确返回错误: %s\n", err)
+		} else {
+			t.Fatalf("token超时未能返回正确的错误: %s\n", err)
+		}
+	} else {
+		t.Fatal("token超时处理不正确")
+	}
 }
