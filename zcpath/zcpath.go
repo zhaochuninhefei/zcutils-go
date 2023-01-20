f612fe35e0d6dea904edc9d7c49f4ac601cf9b29
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-01-20 09:31:16 +0800

    添加zcpath

--- zcpath/zcpath.go
+++ zcpath/zcpath.go
@@ -5,6 +5,10 @@ import (
 	"os"
 )
 
+// CreateDir 创建目录
+//  @param path 目录路径
+//  @return bool 创建成功与否
+//  @return error
 func CreateDir(path string) (bool, error) {
 	if _, err := os.Stat(path); err == nil {
 		return true, fmt.Errorf("该目录已经存在: %s", path)
