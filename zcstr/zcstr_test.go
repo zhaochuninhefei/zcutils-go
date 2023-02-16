65f058d638041cc6b0426b13969cbf2d0dc482c5
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-02-16 11:40:39 +0800

    添加zcstr

--- zcstr/zcstr_test.go
+++ zcstr/zcstr_test.go
@@ -28,3 +28,25 @@ func TestTrimAndUpper(t *testing.T) {
 	assert.Equal(t, exp, rel3)
 
 }
+
+func TestTrimAndLower(t *testing.T) {
+	str1 := " asdf_天行健_FDSA "
+	str2 := "	ASDF_天行健_fdsa  "
+	str3 := "\nASDF_天行健_FDSA\f"
+	exp := "asdf_天行健_fdsa"
+
+	fmt.Printf("str1:[%s]\n", str1)
+	rel1 := TrimAndLower(str1)
+	fmt.Printf("rel1:[%s]\n", rel1)
+	assert.Equal(t, exp, rel1)
+
+	fmt.Printf("str2:[%s]\n", str2)
+	rel2 := TrimAndLower(str2)
+	fmt.Printf("rel2:[%s]\n", rel2)
+	assert.Equal(t, exp, rel2)
+
+	fmt.Printf("str3:[%s]\n", str3)
+	rel3 := TrimAndLower(str3)
+	fmt.Printf("rel3:[%s]\n", rel3)
+	assert.Equal(t, exp, rel3)
+}
