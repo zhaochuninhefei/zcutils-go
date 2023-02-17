7ae233b53cdc79897ec731521d2e3a48367f2684
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-02-17 15:34:02 +0800

    添加zcbitmap

--- zcbitmap/zcbitmap.go
+++ zcbitmap/zcbitmap.go
@@ -78,6 +78,77 @@ func (b *BitSet8) MatchAny(bs BitSet8) bool {
 
 // --------------------------------------------------------------------------
 
+type BitSet16 uint16
+
+// SetBit 将位图中第i位(从右开始)设置为 1
+//  i 一般取值范围: [1, 16], 超过该范围取对16的求余
+func (b *BitSet16) SetBit(i uint) {
+	mask := uint16(1) << ((i - 1) % 16)
+	*b |= BitSet16(mask)
+}
+
+// ClearBit 将位图中第i位(从右开始)设置为 0
+//  i 一般取值范围: [1, 16], 超过该范围取对8的求余
+func (b *BitSet16) ClearBit(i uint) {
+	mask := uint16(1) << ((i - 1) % 16)
+	*b &= BitSet16(^mask)
+}
+
+// CheckBit 检查位图中第 i 位是否为 1
+//  i 一般取值范围: [1, 16], 超过该范围取对8的求余
+func (b *BitSet16) CheckBit(i uint) bool {
+	mask := uint16(1) << ((i - 1) % 16)
+	return (*b & BitSet16(mask)) != 0
+}
+
+// ToInt 将位图转换为一个整数
+func (b *BitSet16) ToInt() uint16 {
+	return uint16(*b)
+}
+
+// ConvBs16FromUInt16 将一个整数转换为位图
+func ConvBs16FromUInt16(i uint16) BitSet16 {
+	return BitSet16(i)
+}
+
+// ToBinaryStr 将位图转换为一个二进制字符串
+func (b *BitSet16) ToBinaryStr(paddingZero bool) string {
+	if paddingZero {
+		return fmt.Sprintf("%016b", b.ToInt())
+	}
+	return strconv.FormatUint(uint64(*b), 2)
+}
+
+// ConvBs16FromBinaryStr 将一个二进制字符串转换为位图
+func ConvBs16FromBinaryStr(s string) BitSet16 {
+	if len(s) > 16 {
+		s = s[len(s)-16:]
+	}
+	i, err := strconv.ParseUint(s, 2, 0)
+	if err != nil {
+		return 0
+	}
+	return BitSet16(i)
+}
+
+// MatchAll 检查当前位图是否完全匹配目标位图的所有非零位。
+//  所谓匹配，即目标位图第i位非0的话，当前位图的第i位也非零。
+//  当前位图与目标位图bs按位与,结果仍等于bs则表示满足MatchAll
+func (b *BitSet16) MatchAll(bs BitSet16) bool {
+	newBs := *b & bs
+	return newBs == bs
+}
+
+// MatchAny 检查当前位图是否有任意一位匹配目标位图对应的非零位。
+//  所谓匹配，即目标位图第i位非0的话，当前位图的第i位也非零。
+//  当前位图与目标位图bs按位与,结果大于0则表示满足MatchAny
+func (b *BitSet16) MatchAny(bs BitSet16) bool {
+	newBs := *b & bs
+	return newBs > 0
+}
+
+// --------------------------------------------------------------------------
+
 // BitSet32 32位的位图
 type BitSet32 uint32
 
