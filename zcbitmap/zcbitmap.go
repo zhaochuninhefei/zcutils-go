e10ad69a4eee4d2bd542167e15ac1cc1f1af40b7
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-02-17 15:28:11 +0800

    添加zcbitmap

--- zcbitmap/zcbitmap.go
+++ zcbitmap/zcbitmap.go
@@ -11,41 +11,41 @@ type BitSet8 uint8
 
 // SetBit 将位图中第i位(从右开始)设置为 1
 //  i 一般取值范围: [1, 8], 超过该范围取对8的求余
-func (b BitSet8) SetBit(i uint) {
+func (b *BitSet8) SetBit(i uint) {
 	mask := uint8(1) << ((i - 1) % 8)
-	b |= BitSet8(mask)
+	*b |= BitSet8(mask)
 }
 
 // ClearBit 将位图中第i位(从右开始)设置为 0
 //  i 一般取值范围: [1, 8], 超过该范围取对8的求余
-func (b BitSet8) ClearBit(i uint) {
+func (b *BitSet8) ClearBit(i uint) {
 	mask := uint8(1) << ((i - 1) % 8)
-	b &= BitSet8(^mask)
+	*b &= BitSet8(^mask)
 }
 
 // CheckBit 检查位图中第 i 位是否为 1
 //  i 一般取值范围: [1, 8], 超过该范围取对8的求余
-func (b BitSet8) CheckBit(i uint) bool {
+func (b *BitSet8) CheckBit(i uint) bool {
 	mask := uint8(1) << ((i - 1) % 8)
-	return (b & BitSet8(mask)) != 0
+	return (*b & BitSet8(mask)) != 0
 }
 
 // ToInt 将位图转换为一个整数
-func (b BitSet8) ToInt() uint8 {
-	return uint8(b)
+func (b *BitSet8) ToInt() uint8 {
+	return uint8(*b)
 }
 
-// ConvBs8FromUint8 将一个整数转换为位图
-func ConvBs8FromUint8(i uint8) BitSet8 {
+// ConvBs8FromUInt8 将一个整数转换为位图
+func ConvBs8FromUInt8(i uint8) BitSet8 {
 	return BitSet8(i)
 }
 
 // ToBinaryStr 将位图转换为一个二进制字符串
-func (b BitSet8) ToBinaryStr(paddingZero bool) string {
+func (b *BitSet8) ToBinaryStr(paddingZero bool) string {
 	if paddingZero {
 		return fmt.Sprintf("%08b", b.ToInt())
 	}
-	return strconv.FormatUint(uint64(b), 2)
+	return strconv.FormatUint(uint64(*b), 2)
 }
 
 // ConvBs8FromBinaryStr 将一个二进制字符串转换为位图
@@ -63,16 +63,16 @@ func ConvBs8FromBinaryStr(s string) BitSet8 {
 // MatchAll 检查当前位图是否完全匹配目标位图的所有非零位。
 //  所谓匹配，即目标位图第i位非0的话，当前位图的第i位也非零。
 //  当前位图与目标位图bs按位与,结果仍等于bs则表示满足MatchAll
-func (b BitSet8) MatchAll(bs BitSet8) bool {
-	newBs := b & bs
+func (b *BitSet8) MatchAll(bs BitSet8) bool {
+	newBs := *b & bs
 	return newBs == bs
 }
 
 // MatchAny 检查当前位图是否有任意一位匹配目标位图对应的非零位。
 //  所谓匹配，即目标位图第i位非0的话，当前位图的第i位也非零。
 //  当前位图与目标位图bs按位与,结果大于0则表示满足MatchAny
-func (b BitSet8) MatchAny(bs BitSet8) bool {
-	newBs := b & bs
+func (b *BitSet8) MatchAny(bs BitSet8) bool {
+	newBs := *b & bs
 	return newBs > 0
 }
 
@@ -83,27 +83,27 @@ type BitSet32 uint32
 
 // SetBit 将位图中第i位(从右开始)设置为 1
 //  i 一般取值范围: [1, 32], 超过该范围取对32的求余
-func (b BitSet32) SetBit(i uint) {
+func (b *BitSet32) SetBit(i uint) {
 	mask := uint32(1) << ((i - 1) % 32)
-	b |= BitSet32(mask)
+	*b |= BitSet32(mask)
 }
 
 // ClearBit 将位图中第i位(从右开始)设置为 0
 //  i 一般取值范围: [1, 32], 超过该范围取对32的求余
-func (b BitSet32) ClearBit(i uint) {
+func (b *BitSet32) ClearBit(i uint) {
 	mask := uint32(1) << ((i - 1) % 32)
-	b &= BitSet32(^mask)
+	*b &= BitSet32(^mask)
 }
 
 // CheckBit 检查位图中第 i 位是否为 1
-func (b BitSet32) CheckBit(i uint) bool {
+func (b *BitSet32) CheckBit(i uint) bool {
 	mask := uint32(1) << ((i - 1) % 32)
-	return (b & BitSet32(mask)) != 0
+	return (*b & BitSet32(mask)) != 0
 }
 
 // ToInt 将位图转换为一个整数
-func (b BitSet32) ToInt() int {
-	return int(b)
+func (b *BitSet32) ToInt() int {
+	return int(*b)
 }
 
 // ConvBs32FromUInt32 将一个整数转换为位图
@@ -112,11 +112,11 @@ func ConvBs32FromUInt32(i uint32) BitSet32 {
 }
 
 // ToBinaryStr 将位图转换为一个二进制字符串
-func (b BitSet32) ToBinaryStr(paddingZero bool) string {
+func (b *BitSet32) ToBinaryStr(paddingZero bool) string {
 	if paddingZero {
 		return fmt.Sprintf("%032b", b.ToInt())
 	}
-	return strconv.FormatUint(uint64(b), 2)
+	return strconv.FormatUint(uint64(*b), 2)
 }
 
 // ConvBs32FromBinaryStr 将一个二进制字符串转换为位图
@@ -134,8 +134,8 @@ func ConvBs32FromBinaryStr(s string) BitSet32 {
 // MatchAll 检查当前位图是否完全匹配目标位图的所有非零位。
 //  所谓匹配，即目标位图第i位非0的话，当前位图的第i位也非零。
 //  当前位图与目标位图bs按位与,结果仍等于bs则表示满足MatchAll
-func (b BitSet32) MatchAll(bs BitSet32) bool {
-	newBs := b & bs
+func (b *BitSet32) MatchAll(bs BitSet32) bool {
+	newBs := *b & bs
 	fmt.Printf("newBs: %s\n", newBs.ToBinaryStr(false))
 	return newBs == bs
 }
@@ -143,8 +143,8 @@ func (b BitSet32) MatchAll(bs BitSet32) bool {
 // MatchAny 检查当前位图是否有任意一位匹配目标位图对应的非零位。
 //  所谓匹配，即目标位图第i位非0的话，当前位图的第i位也非零。
 //  当前位图与目标位图bs按位与,结果大于0则表示满足MatchAny
-func (b BitSet32) MatchAny(bs BitSet32) bool {
-	newBs := b & bs
+func (b *BitSet32) MatchAny(bs BitSet32) bool {
+	newBs := *b & bs
 	fmt.Printf("newBs: %s\n", newBs.ToBinaryStr(false))
 	return newBs > 0
 }
--- zcbitmap/zcbitmap_test.go
+++ zcbitmap/zcbitmap_test.go
@@ -5,6 +5,128 @@ import (
 	"testing"
 )
 
+func TestBitSet8(t *testing.T) {
+	fmt.Println("测试 1000100 :")
+	var b BitSet8
+	b.SetBit(3)
+	b.SetBit(7)
+	fmt.Printf("%08b\n", b.ToInt())
+	fmt.Println(b.ToBinaryStr(true))
+	bStrAct := b.ToBinaryStr(false)
+	fmt.Println(bStrAct)
+	if bStrAct != "1000100" {
+		t.Fatal("测试 1000100 失败")
+	}
+
+	fmt.Println("将第3位设置为0:")
+	b.ClearBit(3)
+	fmt.Printf("%08b\n", b.ToInt())
+	fmt.Println(b.ToBinaryStr(true))
+	fmt.Println(b.ToBinaryStr(false))
+
+	fmt.Printf("第3位是否为1: %t\n", b.CheckBit(3))
+	if b.CheckBit(3) {
+		t.Fatal("将第3位设置为0 失败")
+	}
+	fmt.Printf("第7位是否为1: %t\n", b.CheckBit(7))
+	if !b.CheckBit(7) {
+		t.Fatal("将第7位设置为1 失败")
+	}
+
+	fmt.Println("将 255 转为 BitSet32")
+	var i uint8 = 255
+	b = ConvBs8FromUInt8(i)
+	fmt.Printf("%08b\n", b.ToInt())
+	fmt.Println(b.ToBinaryStr(true))
+	fmt.Println(b.ToBinaryStr(false))
+	if "11111111" != b.ToBinaryStr(false) {
+		t.Fatal("将 255 转为 BitSet32 失败")
+	}
+
+	fmt.Println("将 10101100 转为 BitSet32")
+	s := "10101100"
+	b = ConvBs8FromBinaryStr(s)
+	fmt.Printf("对应int值: %d\n", b)
+	fmt.Printf("%08b\n", b.ToInt())
+	fmt.Println(b.ToBinaryStr(true))
+	fmt.Println(b.ToBinaryStr(false))
+	if "10101100" != b.ToBinaryStr(false) {
+		t.Fatal("将 255 转为 BitSet32 失败")
+	}
+}
+
+func TestMatchBitSet8(t *testing.T) {
+	status := ConvBs8FromBinaryStr("101")
+
+	fmt.Println("----- 测试 111")
+	s := "111"
+	b := ConvBs8FromBinaryStr(s)
+	fmt.Printf("对应int值: %d\n", b)
+	fmt.Println(b.ToBinaryStr(false))
+
+	allMatch := status.MatchAll(b)
+	fmt.Printf("101 MatchAll 111: %t\n", allMatch)
+	if allMatch {
+		t.Fatal("101 MatchAll 111 不应该返回 true")
+	}
+	anyMatch := status.MatchAny(b)
+	fmt.Printf("101 MatchAny 111: %t\n", anyMatch)
+	if !anyMatch {
+		t.Fatal("101 MatchAny 111 不应该返回 false")
+	}
+
+	fmt.Println("----- 测试 100")
+	s = "100"
+	b = ConvBs8FromBinaryStr(s)
+	fmt.Printf("对应int值: %d\n", b)
+	fmt.Println(b.ToBinaryStr(false))
+
+	allMatch = status.MatchAll(b)
+	fmt.Printf("101 MatchAll 100: %t\n", allMatch)
+	if !allMatch {
+		t.Fatal("101 MatchAll 100 不应该返回 false")
+	}
+	anyMatch = status.MatchAny(b)
+	fmt.Printf("101 MatchAny 100: %t\n", anyMatch)
+	if !anyMatch {
+		t.Fatal("101 MatchAny 100 不应该返回 false")
+	}
+
+	fmt.Println("----- 测试 010")
+	s = "010"
+	b = ConvBs8FromBinaryStr(s)
+	fmt.Printf("对应int值: %d\n", b)
+	fmt.Println(b.ToBinaryStr(false))
+
+	allMatch = status.MatchAll(b)
+	fmt.Printf("101 MatchAll 010: %t\n", allMatch)
+	if allMatch {
+		t.Fatal("101 MatchAll 010 不应该返回 true")
+	}
+	anyMatch = status.MatchAny(b)
+	fmt.Printf("101 MatchAny 010: %t\n", anyMatch)
+	if anyMatch {
+		t.Fatal("101 MatchAny 010 不应该返回 true")
+	}
+
+	fmt.Println("----- 测试 001")
+	s = "001"
+	b = ConvBs8FromBinaryStr(s)
+	fmt.Printf("对应int值: %d\n", b)
+	fmt.Println(b.ToBinaryStr(false))
+
+	allMatch = status.MatchAll(b)
+	fmt.Printf("101 MatchAll 001: %t\n", allMatch)
+	if !allMatch {
+		t.Fatal("101 MatchAll 001 不应该返回 false")
+	}
+	anyMatch = status.MatchAny(b)
+	fmt.Printf("101 MatchAny 001: %t\n", anyMatch)
+	if !anyMatch {
+		t.Fatal("101 MatchAny 001 不应该返回 false")
+	}
+}
+
 func TestBitSet32(t *testing.T) {
 	fmt.Println("测试 1000100 :")
 	var b BitSet32
@@ -12,7 +134,11 @@ func TestBitSet32(t *testing.T) {
 	b.SetBit(7)
 	fmt.Printf("%032b\n", b.ToInt())
 	fmt.Println(b.ToBinaryStr(true))
-	fmt.Println(b.ToBinaryStr(false))
+	bStrAct := b.ToBinaryStr(false)
+	fmt.Println(bStrAct)
+	if bStrAct != "1000100" {
+		t.Fatal("测试 1000100 失败")
+	}
 
 	fmt.Println("将第3位设置为0:")
 	b.ClearBit(3)
@@ -21,7 +147,13 @@ func TestBitSet32(t *testing.T) {
 	fmt.Println(b.ToBinaryStr(false))
 
 	fmt.Printf("第3位是否为1: %t\n", b.CheckBit(3))
+	if b.CheckBit(3) {
+		t.Fatal("将第3位设置为0 失败")
+	}
 	fmt.Printf("第7位是否为1: %t\n", b.CheckBit(7))
+	if !b.CheckBit(7) {
+		t.Fatal("将第7位设置为1 失败")
+	}
 
 	fmt.Println("将 255 转为 BitSet32")
 	var i uint32 = 255
@@ -29,6 +161,9 @@ func TestBitSet32(t *testing.T) {
 	fmt.Printf("%032b\n", b.ToInt())
 	fmt.Println(b.ToBinaryStr(true))
 	fmt.Println(b.ToBinaryStr(false))
+	if "11111111" != b.ToBinaryStr(false) {
+		t.Fatal("将 255 转为 BitSet32 失败")
+	}
 
 	fmt.Println("将 10101100 转为 BitSet32")
 	s := "10101100"
@@ -37,7 +172,9 @@ func TestBitSet32(t *testing.T) {
 	fmt.Printf("%032b\n", b.ToInt())
 	fmt.Println(b.ToBinaryStr(true))
 	fmt.Println(b.ToBinaryStr(false))
-
+	if "10101100" != b.ToBinaryStr(false) {
+		t.Fatal("将 255 转为 BitSet32 失败")
+	}
 }
 
 func TestMatchBitSet32(t *testing.T) {
@@ -67,14 +204,14 @@ func TestMatchBitSet32(t *testing.T) {
 	fmt.Println(b.ToBinaryStr(false))
 
 	allMatch = status.MatchAll(b)
-	fmt.Printf("101 MatchAll 111: %t\n", allMatch)
+	fmt.Printf("101 MatchAll 100: %t\n", allMatch)
 	if !allMatch {
-		t.Fatal("101 MatchAll 111 不应该返回 false")
+		t.Fatal("101 MatchAll 100 不应该返回 false")
 	}
 	anyMatch = status.MatchAny(b)
-	fmt.Printf("101 MatchAny 111: %t\n", anyMatch)
+	fmt.Printf("101 MatchAny 100: %t\n", anyMatch)
 	if !anyMatch {
-		t.Fatal("101 MatchAny 111 不应该返回 false")
+		t.Fatal("101 MatchAny 100 不应该返回 false")
 	}
 
 	fmt.Println("----- 测试 010")
@@ -84,14 +221,14 @@ func TestMatchBitSet32(t *testing.T) {
 	fmt.Println(b.ToBinaryStr(false))
 
 	allMatch = status.MatchAll(b)
-	fmt.Printf("101 MatchAll 111: %t\n", allMatch)
+	fmt.Printf("101 MatchAll 010: %t\n", allMatch)
 	if allMatch {
-		t.Fatal("101 MatchAll 111 不应该返回 true")
+		t.Fatal("101 MatchAll 010 不应该返回 true")
 	}
 	anyMatch = status.MatchAny(b)
-	fmt.Printf("101 MatchAny 111: %t\n", anyMatch)
+	fmt.Printf("101 MatchAny 010: %t\n", anyMatch)
 	if anyMatch {
-		t.Fatal("101 MatchAny 111 不应该返回 true")
+		t.Fatal("101 MatchAny 010 不应该返回 true")
 	}
 
 	fmt.Println("----- 测试 001")
@@ -101,13 +238,13 @@ func TestMatchBitSet32(t *testing.T) {
 	fmt.Println(b.ToBinaryStr(false))
 
 	allMatch = status.MatchAll(b)
-	fmt.Printf("101 MatchAll 111: %t\n", allMatch)
+	fmt.Printf("101 MatchAll 001: %t\n", allMatch)
 	if !allMatch {
-		t.Fatal("101 MatchAll 111 不应该返回 false")
+		t.Fatal("101 MatchAll 001 不应该返回 false")
 	}
 	anyMatch = status.MatchAny(b)
-	fmt.Printf("101 MatchAny 111: %t\n", anyMatch)
+	fmt.Printf("101 MatchAny 001: %t\n", anyMatch)
 	if !anyMatch {
-		t.Fatal("101 MatchAny 111 不应该返回 false")
+		t.Fatal("101 MatchAny 001 不应该返回 false")
 	}
 }
