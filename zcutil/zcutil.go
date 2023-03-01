655764451c17c7efbc9cacbfc84f2007e00c776a
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-03-01 18:08:45 +0800

    添加zcutil

---
+++ zcutil/zcutil.go
@@ -0,0 +1,10 @@
+package zcutil
+
+import "encoding/binary"
+
+// Int32ToBytes 将int32值转为4个字节的byte数组
+func Int32ToBytes(i int32) []byte {
+	buf := make([]byte, 4)
+	binary.BigEndian.PutUint32(buf, uint32(i))
+	return buf
+}
