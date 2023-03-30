ac2db06f9803ed7379f5e49bd95fd93199fc31fe
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-03-30 11:22:34 +0800

    添加注释

--- zcstr/zcstr.go
+++ zcstr/zcstr.go
@@ -2,10 +2,12 @@ package zcstr
 
 import "strings"
 
+// TrimAndUpper 去除字符串首尾空格并转为大写
 func TrimAndUpper(str string) string {
 	return strings.ToUpper(strings.TrimSpace(str))
 }
 
+// TrimAndLower 去除字符串首尾空格并转为小写
 func TrimAndLower(str string) string {
 	return strings.ToLower(strings.TrimSpace(str))
 }
