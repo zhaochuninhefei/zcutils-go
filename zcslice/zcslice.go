fe16ca14b1c1e7d9eeb2f700523f3738dfece1dc
zhaochuninhefei <zhaochun.hefei@qq.com>
2023-07-20 11:48:53 +0800

    为zcslice添加Diff函数

--- zcslice/zcslice.go
+++ zcslice/zcslice.go
@@ -9,3 +9,21 @@ func Contains(slice []string, str string) bool {
 	}
 	return false
 }
+
+// Diff 计算两个字符串切片的差集
+func Diff(slice1, slice2 []string) []string {
+	m := make(map[string]bool)
+
+	for _, item := range slice2 {
+		m[item] = true
+	}
+
+	var diff []string
+	for _, item := range slice1 {
+		if _, ok := m[item]; !ok {
+			diff = append(diff, item)
+		}
+	}
+
+	return diff
+}
--- zcslice/zcslice_test.go
+++ zcslice/zcslice_test.go
@@ -1,6 +1,9 @@
 package zcslice
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestContains(t *testing.T) {
 	type args struct {
@@ -33,6 +36,34 @@ func TestContains(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Contains(tt.args.slice, tt.args.str); got != tt.want {
 				t.Errorf("Contains() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDiff(t *testing.T) {
+	type args struct {
+		slice1 []string
+		slice2 []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "test1",
+			args: args{
+				slice1: []string{"a", "b", "c", "甲a", "乙b", "丙c"},
+				slice2: []string{"a", "b", "c"},
+			},
+			want: []string{"甲a", "乙b", "丙c"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Diff(tt.args.slice1, tt.args.slice2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Diff() = %v, want %v", got, tt.want)
 			}
 		})
 	}
